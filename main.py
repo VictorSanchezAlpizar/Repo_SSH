22d72d2ce3117e2cf0a0aca81bc438fffd3a51b7
VictorSanchezAlpizar <vicsma@hotmail.com>
2025-03-27 21:33:55 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,164 @@
+import tkinter as tk
+
+#Parameters --------------------------------------------------------------------------------------
+# Crear la interfaz principal
+root = tk.Tk()
+root.title("Interfaz Principal")
+
+# Crear y colocar los botones en la interfaz principal
+buttons = [
+    '1', '2', '3', 'Esc',
+    '4', '5', '6', 'Enter',
+    '7', '8', '9', 'Pánico',
+    '*', '0', '#', 'Bomberos'
+]
+
+row_custom    = 0
+col_custom    = 0
+current_State = 0
+
+sequence = []
+additional_text_labels = []
+
+#States
+START_MENU          = 0
+MAIN_MENU           = 1
+USER_MODE_MENU      = 2
+MODO_0_MENU         = 3
+MODO_1_MENU         = 4
+MODO_DESARMADO_MENU = 5
+
+#Utils ---------------------------------------------------------------------------------------------
+
+def update_label():
+    """Actualiza el label_ID según el estado actual."""
+    if current_State == START_MENU:
+        label_ID.config(text="User ID: ")
+        hide_all()
+    elif current_State == MAIN_MENU:
+        label_ID.config(text="Opciones: ")
+        hide_all()
+        show_main_menu()
+
+def hide_all():
+    hide_main_menu()
+
+def show_main_menu():
+    """Muestra las líneas de texto adicionales en la pantalla principal."""
+    for label in additional_text_labels:
+        label.grid()
+
+def hide_main_menu():
+    """Oculta las líneas de texto adicionales en la pantalla principal."""
+    for label in additional_text_labels:
+        label.grid_remove()
+
+def on_button_click(value):
+    global current_State, sequence
+
+    if value == "Pánico" or value == "Bomberos":
+        # Cambiar el color del indicador L1 a verde cuando se presiona "Batería"
+        label_alerta.config(bg="red")
+
+    elif value == "Esc":
+        # Limpiar el cuadro de texto si se presiona "Esc"
+        entry_ID.delete(0, tk.END)
+        sequence.clear()  # Limpiar la secuencia
+
+    elif value == "Enter":
+        if sequence == ["1"]:
+            current_State = MAIN_MENU  # Cambiar al menú principal
+            update_label()  # Actualizar el label
+        else:
+            current_State = START_MENU  # Volver al estado inicial
+            update_label()  # Actualizar el label
+        entry_ID.delete(0, tk.END)
+        sequence.clear()  # Limpiar la secuencia
+
+    else:
+        # Añadir el valor del botón al cuadro de texto
+        entry_ID.insert(tk.END, value)
+        sequence.append(value)  # Añadir el valor a la secuencia
+
+    print(f"Button {value} clicked")
+
+def turn_on_main_button(value):
+    if value == "Sim falla electrica ON":
+        # Cambiar el estado del botón en la interfaz principal
+        label_bateria.config(bg="green", text="Batería")
+    if value == "Sim falla electrica OFF":
+        # Cambiar el estado del botón en la interfaz principal
+        label_bateria.config(bg="white", text="Batería")
+    if value == "Sim Bomberos PASS":
+        # Cambiar el estado del botón en la interfaz principal
+        label_alerta.config(bg="white", text="Alerta")
+
+def open_secondary_interface():
+    secondary_window = tk.Toplevel(root)
+    secondary_window.minsize(width=25, height=25)
+    secondary_window.config(padx=5, pady=5)
+    secondary_window.title("Interfaz Secundaria")
+
+    # Botón en la interfaz secundaria para encender el botón en la principal
+    tk.Button(secondary_window, text="Sim falla electrica ON", command=lambda b="Sim falla electrica ON": turn_on_main_button(b)).pack(pady=5)
+    tk.Button(secondary_window, text="Sim falla electrica OFF", command=lambda b="Sim falla electrica OFF": turn_on_main_button(b)).pack(pady=10)
+    tk.Button(secondary_window, text="Sim Sensor falla", command=lambda b="Sim Sensor falla": turn_on_main_button(b)).pack(pady=15)
+    tk.Button(secondary_window, text="Sim Bomberos PASS", command=lambda b="Sim Bomberos PASS": turn_on_main_button(b)).pack(pady=20)
+    
+#Main interface ---------------------------------------------------------------------------------------
+
+label_ID = tk.Label(text="User ID: ", font=("Arial", 14))
+label_ID.grid(column=0, row=0)
+
+entry_ID = tk.Entry(width=10, font=("Arial", 14))
+entry_ID.grid(column=1, row=0)
+
+col_custom+=2
+
+for button in buttons:
+    if button == 'Esc':
+        tk.Button(root, text=button, command=lambda b=button: on_button_click(b), bg='white').grid(row=row_custom, column=col_custom, padx=5, pady=5, sticky="nsew")
+    elif button == 'Enter':
+        tk.Button(root, text=button, command=lambda b=button: on_button_click(b), bg='white').grid(row=row_custom, column=col_custom, padx=5, pady=5, sticky="nsew")
+    elif button == 'Pánico':
+        tk.Button(root, text=button, command=lambda b=button: on_button_click(b), bg='yellow').grid(row=row_custom, column=col_custom, padx=5, pady=5, sticky="nsew")
+    elif button == 'Bomberos':
+        tk.Button(root, text=button, command=lambda b=button: on_button_click(b), bg='red').grid(row=row_custom, column=col_custom, padx=5, pady=5, sticky="nsew")
+    else:
+        tk.Button(root, text=button, command=lambda b=button: on_button_click(b)).grid(row=row_custom, column=col_custom, padx=5, pady=5, sticky="nsew")
+    
+    col_custom += 1
+    if col_custom > 5:
+        col_custom = 2
+        row_custom += 1
+
+# Crear las líneas de texto adicionales (inicialmente ocultas)
+additional_text_labels = [
+    tk.Label(root, text="1. Ver perfil", font=("Arial", 12)),
+    tk.Label(root, text="2. Cambiar contraseña", font=("Arial", 12)),
+    tk.Label(root, text="3. Cerrar sesión", font=("Arial", 12))
+]
+
+# Colocar las líneas de texto adicionales en la interfaz (inicialmente ocultas)
+for i, label in enumerate(additional_text_labels):
+    label.grid(row=row_custom+3+i, column=0, columnspan=4, sticky="w")
+    label.grid_remove()  # Ocultar inicialmente
+
+
+label_bateria = tk.Label(root, text="Batería", bg="white", width=10, height=2)
+label_bateria.grid(row=row_custom+1, column=0, columnspan=1)
+
+label_alerta = tk.Label(root, text="Alerta", bg="white", width=10, height=2)
+label_alerta.grid(row=row_custom+1, column=1, columnspan=2)
+
+# Etiquetas adicionales en la interfaz principal
+tk.Label(root, text="S E S", font=('Arial', 14)).grid(row=row_custom+2, column=0, columnspan=1)
+tk.Label(root, text="SSH-101", font=('Arial', 14)).grid(row=row_custom+2, column=1, columnspan=2)
+
+# Botón para abrir la interfaz secundaria
+tk.Button(root, text="Abrir Interfaz Sim", command=open_secondary_interface).grid(row=row_custom+4, column=0, columnspan=4)
+
+root.mainloop()
+
+
+if ()
---
+++ sensors_list.py
@@ -0,0 +1,87 @@
+import json
+
+#Zone Disponibles
+ZONE_0 = 0
+ZONE_1 = 1
+
+#Status Disponibles
+ACTIVE = 1
+INACTIVE = 0
+
+#Install Estatus
+INSTALL = 1
+NOT_INSTALL = 0
+
+Sensors_list = {
+    "S1": {"ID": 1, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S2": {"ID": 2, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S3": {"ID": 3, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S4": {"ID": 4, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S5": {"ID": 5, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S6": {"ID": 6, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S7": {"ID": 7, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S8": {"ID": 8, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S9": {"ID": 9, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S10": {"ID": 10, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S11": {"ID": 11, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S12": {"ID": 12, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S13": {"ID": 13, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S14": {"ID": 14, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S15": {"ID": 15, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+    "S16": {"ID": 16, "Zone": 0, "Status": INACTIVE, "Install": NOT_INSTALL},
+}
+
+def actualizar_sensor(sensor_name, key, value):
+    valid_config = False
+    if sensor_name in Sensors_list and key in Sensors_list[sensor_name]:
+        Sensors_list[sensor_name][key] = value
+        valid_config = True
+        return valid_config
+    else:
+        print("Configuracion no disponible")
+        return valid_config
+
+def save_sensors_list():
+    with open("sensors.txt", "w") as file:
+        json.dump(Sensors_list, file, indent=4)
+    print("Lista de sensores almacenada en memoria")
+
+def read_sensors_list():
+    with open("sensors.txt", "r") as file:
+        Sensors_list = json.load(file)
+    print("Lista de sensores actualizada:", Sensors_list)
+
+"""
+HOWTO: Guardar y leer de "memoria" la lista de sensores
+
+print(f'Sensor ID: {Sensors_list["S1"]["ID"]}')
+print(f'Sensor Install Status: {Sensors_list["S1"]["Install"]}')
+
+x = actualizar_sensor("S1", "Install", INSTALL)
+print(f'Sensor ID: {Sensors_list["S1"]["ID"]}')
+print(f'Sensor Install Status: {Sensors_list["S1"]["Install"]}')
+print(f'Was config valid: {x}')
+
+save_sensors_list()
+read_sensors_list()
+
+print(f'Sensor ID: {Sensors_list["S1"]["ID"]}')
+print(f'Sensor Install Status: {Sensors_list["S1"]["Install"]}')
+"""
+
+"""
+HOWTO: Acceder lista de sensores
+
+print(f'Sensor ID: {Sensors_list["S1"]["ID"]}')
+print(f'Sensor Install Status: {Sensors_list["S1"]["Install"]}')
+
+x = actualizar_sensor("S1", "Install", INSTALL)
+print(f'Sensor ID: {Sensors_list["S1"]["ID"]}')
+print(f'Sensor Install Status: {Sensors_list["S1"]["Install"]}')
+print(f'Was config valid: {x}')
+
+x = actualizar_sensor("S1", "Installed", INSTALL)
+print(f'Sensor ID: {Sensors_list["S1"]["ID"]}')
+print(f'Sensor Install Status: {Sensors_list["S1"]["Install"]}')
+print(f'Was config valid: {x}')
+"""
